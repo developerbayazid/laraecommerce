95840d4e226ff806b77925dff6e3b8115bc25408
developerbayazid <bayazid.freelancer@gmail.com>
2025-10-14 00:09:12 +0600

    Intigrate Admin panel and home page template

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,7 @@
-<x-app-layout>
+@extends('admin.main')
+
+
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Admin Dashboard') }}
@@ -14,4 +17,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout> --}}
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
